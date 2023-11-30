3583fa6674c6f64a0906cc72b03fec1aacbb1163
Yoga Ardiansyah <yogaardiansyah@protonmail.com>
2023-11-30 17:53:08 +0700

    Update anime_recommendation_app.py

--- anime_recommendation_app.py
+++ anime_recommendation_app.py
@@ -13,6 +13,104 @@ model_filename = 'animeR.sav'
 with open(model_filename, 'rb') as model_file:
     model_ml = pickle.load(model_file)
 
+# Data
+
+# Mengganti nilai kosong 'start_season_season' dengan label 'unknown'
+data['start_season_season'].fillna('unknown', inplace=True)
+
+# Mengisi nilai kosong pada 'rating' dengan 'r'
+data['rating'].fillna('r', inplace=True)
+
+# Mengisi nilai kosong pada 'source' dengan 'original'
+data['source'].fillna('original', inplace=True)
+
+# Melihat jumlah unik setelah pengisian nilai kosong
+print("Unique Values in 'start_season_season' After Filling NaN:")
+print(data['start_season_season'].unique())
+
+print("\nUnique Values in 'rating' After Filling NaN:")
+print(data['rating'].unique())
+
+print("\nUnique Values in 'source' After Filling NaN:")
+print(data['source'].unique())
+
+# Mapping data 'status' menjadi numeric
+
+# Melihat nilai unik sebelum perubahan
+print("Unique Values in 'status' (Before):")
+print(data['status'].unique())
+
+# Membuat peta untuk mengganti nilai string dengan integer
+status_mapping = {'finished_airing': 1, 'currently_airing': 2, 'not_yet_aired': 3}
+
+# Mengganti nilai string dengan integer
+data['status'] = data['status'].map(status_mapping)
+
+# Melihat nilai unik setelah perubahan
+print("\nUnique Values in 'status' (After):")
+print(data['status'].unique())
+
+# Melihat nilai unik sebelum perubahan
+print("Unique Values in 'media_type' (Before):")
+print(data['media_type'].unique())
+
+# Membuat peta untuk mengganti nilai string dengan integer
+media_type_mapping = {'tv': 1, 'movie': 2, 'ova': 3, 'ona': 4, 'music': 5, 'special': 6, 'unknown': 7}
+
+# Mengganti nilai string dengan integer
+data['media_type'] = data['media_type'].map(media_type_mapping)
+
+# Melihat nilai unik setelah perubahan
+print("\nUnique Values in 'media_type' (After):")
+print(data['media_type'].unique())
+
+# Melihat nilai unik sebelum perubahan
+print("Unique Values in 'source' (Before):")
+print(data['source'].unique())
+
+# Membuat peta untuk mengganti nilai string dengan integer
+source_mapping = {
+    'manga': 1, 'visual_novel': 2, 'original': 3, 'light_novel': 4,
+    'web_manga': 5, 'novel': 6, '4_koma_manga': 7, 'game': 8,
+    'other': 9, 'web_novel': 10, 'mixed_media': 11, 'music': 12,
+    'card_game': 13, 'book': 14, 'picture_book': 15, 'radio': 16
+}
+
+# Mengganti nilai string dengan integer
+data['source'] = data['source'].map(source_mapping)
+
+# Melihat nilai unik setelah perubahan
+print("\nUnique Values in 'source' (After):")
+print(data['source'].unique())
+
+# Melihat nilai unik sebelum perubahan
+print("Unique Values in 'start_season_season' (Before):")
+print(data['start_season_season'].unique())
+
+# Membuat peta untuk mengganti nilai string dengan integer
+season_mapping = {'spring': 1, 'fall': 2, 'winter': 3, 'summer': 4}
+
+# Mengganti nilai string dengan integer
+data['start_season_season'] = data['start_season_season'].map(season_mapping)
+
+# Melihat nilai unik setelah perubahan
+print("\nUnique Values in 'start_season_season' (After):")
+print(data['start_season_season'].unique())
+
+# Melihat nilai unik sebelum perubahan
+print("Unique Values in 'rating' (Before):")
+print(data['rating'].unique())
+
+# Membuat peta untuk mengganti nilai string dengan integer
+rating_mapping = {'r': 1, 'pg_13': 2, 'r+': 3, 'pg': 4, 'g': 5, 'rx': 6}
+
+# Mengganti nilai string dengan integer
+data['rating'] = data['rating'].map(rating_mapping)
+
+# Melihat nilai unik setelah perubahan
+print("\nUnique Values in 'rating' (After):")
+print(data['rating'].unique())
+
 # Daftar semua genre yang ada
 all_genres = [
     'Action', 'Adventure', 'Avant Garde', 'Award Winning', 'Boys Love',
