b0974a38126c2fb9c72db819e72d138d0f5de1d2
Yoga Ardiansyah <yogaardiansyah@protonmail.com>
2023-11-30 18:32:32 +0700

    Update anime_recommendation_app.py

--- anime_recommendation_app.py
+++ anime_recommendation_app.py
@@ -7,7 +7,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 # Load the anime dataset
 url = "https://raw.githubusercontent.com/yogaardiansyah/ML-AnimeR/main/anime.csv_exported.csv"
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def load_data():
     return pd.read_csv(url)
 
@@ -17,6 +17,51 @@ data = load_data()
 model_filename = 'animeR.sav'
 with open(model_filename, 'rb') as model_file:
     model_ml = pickle.load(model_file)
+
+# Mengganti nilai kosong 'start_season_season' dengan label 'unknown'
+data['start_season_season'].fillna('unknown', inplace=True)
+
+# Mengisi nilai kosong pada 'rating' dengan 'r'
+data['rating'].fillna('r', inplace=True)
+
+# Mengisi nilai kosong pada 'source' dengan 'original'
+data['source'].fillna('original', inplace=True)
+
+# Mapping data 'status' menjadi numeric
+# Membuat peta untuk mengganti nilai string dengan integer
+status_mapping = {'finished_airing': 1, 'currently_airing': 2, 'not_yet_aired': 3}
+
+# Mengganti nilai string dengan integer
+data['status'] = data['status'].map(status_mapping)
+
+# Membuat peta untuk mengganti nilai string dengan integer
+media_type_mapping = {'tv': 1, 'movie': 2, 'ova': 3, 'ona': 4, 'music': 5, 'special': 6, 'unknown': 7}
+
+# Mengganti nilai string dengan integer
+data['media_type'] = data['media_type'].map(media_type_mapping)
+
+# Membuat peta untuk mengganti nilai string dengan integer
+source_mapping = {
+    'manga': 1, 'visual_novel': 2, 'original': 3, 'light_novel': 4,
+    'web_manga': 5, 'novel': 6, '4_koma_manga': 7, 'game': 8,
+    'other': 9, 'web_novel': 10, 'mixed_media': 11, 'music': 12,
+    'card_game': 13, 'book': 14, 'picture_book': 15, 'radio': 16
+}
+
+# Mengganti nilai string dengan integer
+data['source'] = data['source'].map(source_mapping)
+
+# Membuat peta untuk mengganti nilai string dengan integer
+season_mapping = {'spring': 1, 'fall': 2, 'winter': 3, 'summer': 4}
+
+# Mengganti nilai string dengan integer
+data['start_season_season'] = data['start_season_season'].map(season_mapping)
+
+# Membuat peta untuk mengganti nilai string dengan integer
+rating_mapping = {'r': 1, 'pg_13': 2, 'r+': 3, 'pg': 4, 'g': 5, 'rx': 6}
+
+# Mengganti nilai string dengan integer
+data['rating'] = data['rating'].map(rating_mapping)
 
 # Daftar semua genre yang ada
 all_genres = [
@@ -46,6 +91,9 @@ scaler_ml = StandardScaler()
 features_ml = data[all_genres + ['media_type', 'mean', 'rating', 'start_season_year']]
 features_scaled_ml = scaler_ml.fit_transform(features_ml)
 
+# Calculate cosine similarity matrix for content-based recommendation
+cosine_sim = calculate_cosine_similarity(features_scaled_ml)
+
 # Define a cache for the cosine similarity matrix
 @st.cache
 def calculate_cosine_similarity(features_scaled_ml):
@@ -60,8 +108,6 @@ def content_based_recommendation(title, cosine_sim, df, num_recommendations=5, g
     scaler_ml = StandardScaler()
     features_scaled_ml = scaler_ml.fit_transform(features_ml)
 
-    # Calculate cosine similarity matrix using the cached function
-    cosine_sim = calculate_cosine_similarity(features_scaled_ml)
 
     user_features = features_ml[df['title'] == title]
     if user_features.empty:
