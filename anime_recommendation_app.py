8cc1bbdf14a98bebac82e00d587e7ac03c9a1153
Yoga Ardiansyah <yogaardiansyah@protonmail.com>
2023-11-30 22:10:08 +0700

    Update anime_recommendation_app.py

--- anime_recommendation_app.py
+++ anime_recommendation_app.py
@@ -59,6 +59,18 @@ def calculate_cosine_similarity(data, all_genres):
 
 # Function to get content-based recommendations using remapped data
 def content_based_recommendation_with_ml(user_data, title, num_recommendations=5, genre_weight=2):
+
+    # Remap user data for recommendation processing
+user_likes_info_remapped = remap_user_data(user_likes_info_original.copy())
+user_features_remapped = user_likes_info_remapped[all_genres + ['media_type', 'mean', 'rating', 'start_season_year']]
+
+# Map categorical data to numeric values for user input (remapped)
+user_data_remapped = remap_user_data(user_data.copy())
+
+# Get features of the user's selected similar anime (remapped)
+user_features_remapped = user_data_remapped[all_genres + ['media_type', 'mean', 'rating', 'start_season_year']]
+
+
     # Calculate cosine similarity matrix using original data
     cosine_sim = calculate_cosine_similarity(data, all_genres)
 
