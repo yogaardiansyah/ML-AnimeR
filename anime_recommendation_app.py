ab5b1e2dd0dc49d8681735f74cd2b2f1c04626fe
Yoga Ardiansyah <yogaardiansyah@protonmail.com>
2023-11-30 20:07:51 +0700

    Update anime_recommendation_app.py

--- anime_recommendation_app.py
+++ anime_recommendation_app.py
@@ -66,7 +66,6 @@ def calculate_cosine_similarity(data):
     return cosine_sim
 
 # Function to get content-based recommendations
-# Function to get content-based recommendations
 def content_based_recommendation(title, num_recommendations=5, genre_weight=2):
     # Ensure the input is a string
     title = str(title)
@@ -111,21 +110,9 @@ def content_based_recommendation(title, num_recommendations=5, genre_weight=2):
 
     return recommended_films
 
-# Function to get similar titles based on a simple string match
-def search_similar_titles(user_input, num_similar_titles=5):
-    # Check if the user input exists in the dataset
-    if user_input not in data['title'].values:
-        st.warning(f"No information found for the anime: {user_input}")
-        return []
-
-    # Get similar titles based on a simple string match
-    similar_titles = data[data['title'].str.contains(user_input, case=False)]['title'].tolist()
-
-    return similar_titles[:num_similar_titles]
-
 # Function to get a random anime title
 def get_random_title():
-    return random.choice(data['title'])
+    return np.random.choice(data['title'])
 
 # Streamlit app
 st.title("Anime Recommendation App")
@@ -133,12 +120,7 @@ st.title("Anime Recommendation App")
 # User input for anime title
 user_input = st.text_input("Enter the name of an anime:")
 
-# Button to trigger recommendations for a random title
-if st.button("Get Recommendations for Random Title"):
-    user_input = get_random_title()
-    st.write(f"Randomly Selected Title: {user_input}")
-
-# Button to trigger recommendations
+# Button to trigger recommendations for user input
 if st.button("Get Recommendations"):
     if user_input:
         # Display similar titles
@@ -146,23 +128,33 @@ if st.button("Get Recommendations"):
         if similar_titles:
             selected_title = st.selectbox("Select a similar title:", similar_titles)
 
-            if st.button("Get Recommendations for Similar Title"):
-                # Display information for the selected title
-                user_likes_info = data[data['title'] == selected_title]
-                if not user_likes_info.empty:
-                    st.subheader(f"Information for {selected_title}:")
-                    st.table(user_likes_info)
+            # Display information for the selected title
+            user_likes_info, recommendations = content_based_recommendation(selected_title)
+            if not user_likes_info.empty:
+                st.subheader(f"Information for {selected_title}:")
+                st.table(user_likes_info)
 
-                    # Get and display recommendations
-                    recommendations = content_based_recommendation(selected_title)
-                    if not recommendations.empty:
-                        st.subheader(f"Recommended Anime for {selected_title}:")
-                        st.table(recommendations[['title', 'genres', 'media_type', 'mean', 'rating', 'start_season_year']])
-                    else:
-                        st.warning(f"No recommendations found for {selected_title}")
-                else:
-                    st.warning(f"No information found for the anime: {selected_title}")
+                st.subheader(f"Recommended Anime for {selected_title}:")
+                st.table(recommendations[['title', 'genres', 'media_type', 'mean', 'rating', 'start_season_year']])
+            else:
+                st.warning(f"No information found for the anime: {selected_title}")
         else:
             st.warning(f"No similar titles found for the anime: {user_input}")
     else:
         st.warning("Please enter the name of an anime.")
+
+# Button to trigger recommendations for a random title
+if st.button("Get Recommendations for Random Title"):
+    # Get a random title
+    random_title = get_random_title()
+
+    # Display information for the selected title
+    user_likes_info, recommendations = content_based_recommendation(random_title)
+    if not user_likes_info.empty:
+        st.subheader(f"Information for Randomly Selected Title: {random_title}:")
+        st.table(user_likes_info)
+
+        st.subheader(f"Recommended Anime for {random_title}:")
+        st.table(recommendations[['title', 'genres', 'media_type', 'mean', 'rating', 'start_season_year']])
+    else:
+        st.warning(f"No information found for the anime: {random_title}")
