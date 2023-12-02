0c97bdd847a2937f847d7f4111431d35f62a0436
Yoga Ardiansyah <yogaardiansyah@protonmail.com>
2023-12-02 18:44:58 +0700

    Update recommendation_model.py

--- recommendation_model.py
+++ recommendation_model.py
@@ -4,16 +4,14 @@ import joblib
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics.pairwise import cosine_similarity
 
-def content_based_recommendation(title, cosine_sim, df, user_preferences, num_recommendations=5, genre_weight=2):
-    # Get features for machine learning model
+def content_based_recommendation(title, cosine_sim, df, user_preferences, all_genres, num_recommendations=5, genre_weight=2):
     features_ml = df[all_genres + ['media_type', 'mean', 'rating', 'start_season_year']]
   
-    # Normalize feature scales using StandardScaler
     scaler_ml = StandardScaler()
     features_scaled_ml = scaler_ml.fit_transform(features_ml)
 
-    # Calculate cosine similarity between the user's preferred anime and all others
-    user_features = features_ml[df['title'] == user_preferences]
+    user_features = get_user_features(df, user_preferences, features_ml, scaler_ml)
+
     if user_features.empty:
         print(f"No information found for the anime: {user_preferences}")
         return pd.DataFrame(), pd.DataFrame()  
@@ -22,16 +20,21 @@ def content_based_recommendation(title, cosine_sim, df, user_preferences, num_re
 
     sim_scores = cosine_similarity(user_features_scaled, features_scaled_ml)
 
-    # Modify the scoring to give higher weight to genre similarity
-    sim_scores = sorted(enumerate(sim_scores[0]), key=lambda x: (x[1] + genre_weight * sum(g in user_genres for g in df['genres'].iloc[x[0]].split(','))), reverse=True)
-    sim_scores = sim_scores[1:(num_recommendations + 1)]
-    film_indices = [i[0] for i in sim_scores]
+    sim_scores = sort_and_filter_recommendations(sim_scores, df, user_genres, num_recommendations, genre_weight)
 
-    # Filter recommended films based on improved genre matching
-    recommended_films = df.iloc[film_indices]
+    recommended_films = df.iloc[sim_scores]
 
     return user_features, recommended_films
-  
+
+def get_user_features(df, user_preferences, features_ml, scaler_ml):
+    user_features = features_ml[df['title'] == user_preferences]
+    return user_features if not user_features.empty else pd.DataFrame()
+
+def sort_and_filter_recommendations(sim_scores, df, user_genres, num_recommendations, genre_weight):
+    sim_scores = sorted(enumerate(sim_scores[0]), key=lambda x: (x[1] + genre_weight * sum(g in user_genres for g in df['genres'].iloc[x[0]].split(','))), reverse=True)
+    film_indices = [i[0] for i in sim_scores[1:(num_recommendations + 1)]]
+    return film_indices
+
 def load_model_and_scaler():
     model_ml = joblib.load('anime_recommendation_model.joblib')
     scaler_ml = joblib.load('anime_scaler.joblib')
@@ -48,7 +51,3 @@ def preprocess_data(data, all_genres):
     features_scaled_ml = scaler_ml.fit_transform(features_ml)
     cosine_sim = cosine_similarity(features_scaled_ml, features_scaled_ml)
     return cosine_sim
-
-def get_user_genres(user_likes, data):
-    user_genres = data[data['title'] == user_likes]['genres'].values[0].split(',')
-    return user_genres
