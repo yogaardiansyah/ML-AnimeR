7db02e44597b1ea0205d54581318a64d99726977
Yoga Ardiansyah <yogaardiansyah@protonmail.com>
2023-12-02 18:47:56 +0700

    Update anime_recommendation_app.py

--- anime_recommendation_app.py
+++ anime_recommendation_app.py
@@ -1,13 +1,12 @@
-# app.py
+# anime_recommendation_app.py
 import streamlit as st
-from recommendation_model import content_based_recommendation, load_model_and_scaler, load_data, preprocess_data, get_user_genres
-from anime_data import display_tabulated_dataframe, get_all_genres
+from recommendation_model import content_based_recommendation, load_model_and_scaler, load_data, preprocess_data
 
 @st.cache(allow_output_mutation=True)
 def load_cached_data():
     model_ml, scaler_ml = load_model_and_scaler()
     data = load_data()
-    all_genres = get_all_genres()
+    all_genres = get_all_genres()  # Assuming get_all_genres is defined in anime_data.py
     cosine_sim = preprocess_data(data, all_genres)
     return model_ml, scaler_ml, data, cosine_sim, all_genres
 
@@ -18,8 +17,6 @@ def main():
 
     st.sidebar.header("User Input")
     user_likes = st.sidebar.selectbox("Select an anime you like:", data['title'].unique())
-
-    user_genres = get_user_genres(user_likes, data)
 
     user_likes_info, recommendations = content_based_recommendation(
         user_likes, cosine_sim, data, user_likes, all_genres, num_recommendations=5, genre_weight=2
