b4627ce76dfa6803a8d9af459e5d0d06287a8f92
Yoga Ardiansyah <yogaardiansyah@protonmail.com>
2023-12-02 18:52:47 +0700

    Update recommendation_model.py

--- recommendation_model.py
+++ recommendation_model.py
@@ -6,25 +6,32 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 def content_based_recommendation(title, cosine_sim, df, user_preferences, all_genres, num_recommendations=5, genre_weight=2):
     features_ml = df[all_genres + ['media_type', 'mean', 'rating', 'start_season_year']]
-  
-    scaler_ml = StandardScaler()
-    features_scaled_ml = scaler_ml.fit_transform(features_ml)
 
-    user_features = get_user_features(df, user_preferences, features_ml, scaler_ml, all_genres)
+    try:
+        scaler_ml = StandardScaler()
+        features_scaled_ml = scaler_ml.fit_transform(features_ml)
 
-    if user_features.empty:
-        print(f"No information found for the anime: {user_preferences}")
-        return pd.DataFrame(), pd.DataFrame()  
+        user_features = get_user_features(df, user_preferences, features_ml, scaler_ml, all_genres)
 
-    user_features_scaled = scaler_ml.transform(user_features)
+        if user_features.empty:
+            print(f"No information found for the anime: {user_preferences}")
+            return pd.DataFrame(), pd.DataFrame()
 
-    sim_scores = cosine_similarity(user_features_scaled, features_scaled_ml)
+        user_features_scaled = scaler_ml.transform(user_features)
 
-    sim_scores = sort_and_filter_recommendations(sim_scores, df, all_genres, num_recommendations, genre_weight)
+        sim_scores = cosine_similarity(user_features_scaled, features_scaled_ml)
 
-    recommended_films = df.iloc[sim_scores]
+        sim_scores = sort_and_filter_recommendations(sim_scores, df, all_genres, num_recommendations, genre_weight)
 
-    return user_features, recommended_films
+        recommended_films = df.iloc[sim_scores]
+
+        return user_features, recommended_films
+
+    except Exception as e:
+        print(f"Error: {e}")
+        print(f"Columns in data: {df.columns}")
+        print(f"Requested columns: {all_genres + ['media_type', 'mean', 'rating', 'start_season_year']}")
+        raise e
 
 def get_user_features(df, user_preferences, features_ml, scaler_ml, all_genres):
     user_features = features_ml[df['title'] == user_preferences]
